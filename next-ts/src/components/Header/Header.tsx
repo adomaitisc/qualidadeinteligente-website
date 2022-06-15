e2ddeae6862f924e55c7c6a77fa78f64984f97e7
Cauã Adomaitis <caadomaitis@gmail.com>
2022-06-15 03:23:40 -0300

    Added Header component

---
+++ next-ts/src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+// import { header } from "../../Data";
+// import Qualidade from "../../assets/Qualidade";
+import styles from "../../styles/components/Header.module.css";
+
+export default function Header() {
+  return (
+    <div className={styles.header}>
+      {/* <Qualidade className={styles.icon} /> */}
+      <div>
+        <ul className={styles.list}>
+          <li className={styles.item}>Soluções</li>
+          <li className={styles.item}>Quem Somos</li>
+          <li className={styles.item}>Explorar</li>
+        </ul>
+      </div>
+    </div>
+  );
+}

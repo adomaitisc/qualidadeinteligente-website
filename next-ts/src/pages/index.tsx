1b9ed3f83ecd83bab32ea41911dd0caec8e1782c
Cauã Adomaitis <caadomaitis@gmail.com>
2022-06-15 05:01:51 -0300

    Added head tags for SEO and OpenGraph/Twitter Cards

--- next-ts/src/pages/index.tsx
+++ next-ts/src/pages/index.tsx
@@ -13,13 +13,56 @@ const Home: NextPage = () => {
     <div className={styles.container}>
       <Head>
         <title>
-          Compromisso com a sua Certificação! | Qualidade Inteligente
+          Qualidade Inteligente | Compromisso com a sua Certificação
         </title>
         <meta
-          name="description"
-          content="Preparamos uma Consultoria Personalizada, adaptada a gestão da sua empresa.Elaboramos a documentação necessária para atender aos requisitos das normas usando uma estrutura simplificada e desburocratizada."
+          name="keywords"
+          content="ISO 9001, ISO 45001, ISO 14001, Segurança do Trabalho, Qualidade, Certificação, Auditorias, PBQP-H, NBR 15.575, Normas Ambientais"
+          key="keywords"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Preparamos uma Consultoria Personalizada, adaptada a gestão da sua empresa. Elaboramos a documentação necessária para atender aos requisitos das normas usando uma estrutura simplificada e desburocratizada."
+          key="description"
+        />
+        <link rel="icon" href="/favicon.ico" key="icon" />
+
+        {/* Open Graph */}
+        <meta
+          property="og:title"
+          content="Qualidade Inteligente | Compromisso com a sua Certificação"
+          key="og:title"
+        />
+        <meta
+          property="og:url"
+          content="https://qualidadeinteligente-website.vercel.app"
+          key="og:url"
+        />
+        <meta property="og:image" content="/thumbnail.jpeg" key="og:image" />
+        <meta
+          property="og:description"
+          content="Preparamos uma Consultoria Personalizada, adaptada a gestão da sua empresa. Elaboramos a documentação necessária para atender aos requisitos das normas usando uma estrutura simplificada e desburocratizada."
+          key="og:description"
+        />
+        <meta property="og:type" content="website" key="og:type" />
+
+        {/* Twitter Card */}
+        <meta name="twitter:card" content="summary" key="twitter:card" />
+        <meta
+          property="twitter:title"
+          content="Qualidade Inteligente | Compromisso com a sua Certificação"
+          key="twitter:title"
+        />
+        <meta
+          name="twitter:image"
+          content="/thumbnail.jpeg"
+          key="twitter:image"
+        />
+        <meta
+          property="twitter:description"
+          content="Preparamos uma Consultoria Personalizada, adaptada a gestão da sua empresa. Elaboramos a documentação necessária para atender aos requisitos das normas usando uma estrutura simplificada e desburocratizada."
+          key="twitter:description"
+        />
       </Head>
 
       <Header />

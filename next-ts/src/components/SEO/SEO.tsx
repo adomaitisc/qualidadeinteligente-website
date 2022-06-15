23c02f860890cf4314fa58e4da5013206e878522
Cauã Adomaitis <caadomaitis@gmail.com>
2022-06-15 05:40:02 -0300

    Metadata and Head on its own SEO component

---
+++ next-ts/src/components/SEO/SEO.tsx
@@ -0,0 +1,64 @@
+import Head from "next/head";
+
+const title = "Qualidade Inteligente | Compromisso com a sua Certificação!";
+const description =
+  "Preparamos uma Consultoria Personalizada, adaptada a gestão da sua empresa. Elaboramos a documentação necessária para atender aos requisitos das normas usando uma estrutura simplificada e desburocratizada.";
+const image =
+  "http://www.qualidadeinteligente.com/favicons/apple-touch-icon.png";
+const metaTags = [
+  {
+    name: "keywords",
+    content:
+      "ISO 9001, ISO 45001, ISO 14001, Segurança do Trabalho, Qualidade, Certificação, Auditorias, PBQP-H, NBR 15.575, Normas Ambientais",
+    key: "keywords",
+  },
+  {
+    name: "description",
+    content: description,
+    key: "description",
+  },
+  { name: "twitter:card", content: "summary", key: "twitterCard" },
+  {
+    property: "twitter:title",
+    content: title,
+    key: "twitterTitle",
+  },
+  {
+    name: "twitter:image",
+    content: image,
+  },
+  {
+    property: "twitter:description",
+    content: description,
+    key: "twitterDescription",
+  },
+  {
+    property: "og:title",
+    content: "Qualidade Inteligente | Compromisso com a sua Certificação!",
+    key: "ogTitle",
+  },
+  { property: "og:description", content: description, key: "ogDescription" },
+  { property: "og:type", content: "website", key: "ogType" },
+  {
+    property: "og:url",
+    content: "https://qualidadeinteligente-website.vercel.app",
+    key: "ogUrl",
+  },
+  {
+    property: "og:image",
+    content: image,
+    key: "ogImage",
+  },
+];
+
+export default function SEO() {
+  return (
+    <Head>
+      <title>{title}</title>
+      <link rel="icon" href="/favicon.ico" />
+      {metaTags.map((tag) => (
+        <meta {...tag} />
+      ))}
+    </Head>
+  );
+}
--- next-ts/src/pages/index.tsx
+++ next-ts/src/pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import SEO from "../components/SEO";
 
 //Importing components
 import Header from "../components/Header";
@@ -8,62 +7,12 @@ import Landing from "../components/Landing";
 import Solutions from "../components/Solutions";
 import Data from "../components/Data";
 
+import styles from "../styles/Home.module.css";
+
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>
-          Qualidade Inteligente | Compromisso com a sua Certificação
-        </title>
-        <meta
-          name="keywords"
-          content="ISO 9001, ISO 45001, ISO 14001, Segurança do Trabalho, Qualidade, Certificação, Auditorias, PBQP-H, NBR 15.575, Normas Ambientais"
-          key="keywords"
-        />
-        <meta
-          name="description"
-          content="Preparamos uma Consultoria Personalizada, adaptada a gestão da sua empresa. Elaboramos a documentação necessária para atender aos requisitos das normas usando uma estrutura simplificada e desburocratizada."
-          key="description"
-        />
-        <link rel="icon" href="/favicon.ico" key="icon" />
-
-        {/* Open Graph */}
-        <meta
-          property="og:title"
-          content="Qualidade Inteligente | Compromisso com a sua Certificação"
-          key="og:title"
-        />
-        <meta
-          property="og:url"
-          content="https://qualidadeinteligente-website.vercel.app"
-          key="og:url"
-        />
-        <meta property="og:image" content="/thumbnail.jpeg" key="og:image" />
-        <meta
-          property="og:description"
-          content="Preparamos uma Consultoria Personalizada, adaptada a gestão da sua empresa. Elaboramos a documentação necessária para atender aos requisitos das normas usando uma estrutura simplificada e desburocratizada."
-          key="og:description"
-        />
-        <meta property="og:type" content="website" key="og:type" />
-
-        {/* Twitter Card */}
-        <meta name="twitter:card" content="summary" key="twitter:card" />
-        <meta
-          property="twitter:title"
-          content="Qualidade Inteligente | Compromisso com a sua Certificação"
-          key="twitter:title"
-        />
-        <meta
-          name="twitter:image"
-          content="/thumbnail.jpeg"
-          key="twitter:image"
-        />
-        <meta
-          property="twitter:description"
-          content="Preparamos uma Consultoria Personalizada, adaptada a gestão da sua empresa. Elaboramos a documentação necessária para atender aos requisitos das normas usando uma estrutura simplificada e desburocratizada."
-          key="twitter:description"
-        />
-      </Head>
+      <SEO />
 
       <Header />
       <Landing />

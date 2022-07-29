33bfdadc4b6914c176c389ca0bffc45876bbc343
Cauã Adomaitis <caadomaitis@gmail.com>
2022-07-29 12:45:54 -0300

    header hover dropdown is gucci

--- next-ts-tailwind/src/components/Header/Header.tsx
+++ next-ts-tailwind/src/components/Header/Header.tsx
@@ -1,9 +1,11 @@
 import Qualidade from "../Assets/Qualidade";
 
-type ItemCardProps = {
+type ItemDropdownProps = {
   title: string;
-  description: string;
-  documentation: string;
+  fLine: string;
+  sLine: string;
+  url: string;
+  rounded: "bottom" | "top" | "none";
 };
 
 export default function Header() {
@@ -20,37 +22,25 @@ export default function Header() {
           </a>
         </li>
         <li className=" flex justify-center group relative">
-          <p className="px-4">Soluções</p>
-          <div className="hidden absolute pt-8 w-16 h-4 group-hover:block"></div>
-          <ul className="hidden absolute rounded-md text-left mt-8 group-hover:block">
-            <li className="px-12 pt-12 rounded-t-xl bg-zinc-900/30 block whitespace-nowrap backdrop-blur-3xl">
-              <a
-                href="#"
-                className="text-base ease-in-out duration-300 hover:text-zinc-400"
-              >
-                ISO 9001 +
-              </a>
-              <div className="mt-2" />
-              <p className="text-zinc-500 font-light leading-relaxed">
-                Se trata da Gestão da Qualidade em qualquer
-                <br />
-                segmento do mercado
-              </p>
-            </li>
-            <li className="px-12 py-12 rounded-b-xl bg-zinc-900/30 block whitespace-nowrap backdrop-blur-3xl">
-              <a
-                href="#"
-                className="text-base ease-in-out duration-300 hover:text-zinc-400"
-              >
-                ISO 45001 +
-              </a>
-              <div className="mt-2" />
-              <p className="text-zinc-500 font-light leading-relaxed">
-                Orientação para melhorar a segurança e saúde
-                <br />
-                dos trabalhadores.
-              </p>
-            </li>
+          <p className="px-4 ease-in-out duration-300 group-hover:text-zinc-500">
+            Soluções
+          </p>
+          <div className="none absolute pt-8 w-16 h-4 group-hover:block"></div>
+          <ul className="invisible scale-90 opacity-0 absolute rounded-md text-left mt-8 transform transition ease-in-out duration-300 group-hover:visible group-hover:opacity-100 group-hover:scale-100">
+            <ItemDropdown
+              title="ISO 9001"
+              fLine="Se trata da Gestão da Qualidade em qualquer"
+              sLine="segmento do mercado."
+              url="#"
+              rounded="top"
+            />
+            <ItemDropdown
+              title="ISO 45001"
+              fLine="Orientação para melhorar a segurança e saúde"
+              sLine="dos trabalhadores."
+              url="#"
+              rounded="bottom"
+            />
           </ul>
         </li>
         <li>
@@ -65,3 +55,36 @@ export default function Header() {
     </div>
   );
 }
+
+const ItemDropdown = ({
+  title,
+  fLine,
+  sLine,
+  url,
+  rounded,
+}: ItemDropdownProps) => {
+  return (
+    <li
+      className={`${
+        rounded === `bottom`
+          ? `rounded-b-xl pt-4 pb-12`
+          : rounded === `top`
+          ? `rounded-t-xl pt-12 pb-4`
+          : null
+      } px-12 bg-zinc-900 block whitespace-nowrap`}
+    >
+      <a
+        href={url}
+        className="text-base ease-in-out duration-300 hover:text-zinc-400"
+      >
+        {title}
+      </a>
+      <div className="mt-2" />
+      <p className="text-zinc-500 font-light leading-relaxed">
+        {fLine}
+        <br />
+        {sLine}
+      </p>
+    </li>
+  );
+};

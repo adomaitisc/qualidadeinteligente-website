ca3985c6bd31db851692475e4bc314ff46f6f3ca
adomaitisc <caadomaitis@gmail.com>
2023-12-26 01:52:43 -0300

    hm

--- src/app/(cms)/admin/(posts)/layout.tsx
+++ src/app/(cms)/admin/(posts)/layout.tsx
@@ -2,9 +2,9 @@ import { AdminHeader } from "./components/admin-header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <body>
       <AdminHeader />
       {children}
-    </>
+    </body>
   );
 }
